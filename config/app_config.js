b5664484821f8a24d82ba3bb44e0d31f507ca3bf
paulosilva92 <paulorob_s@hotmail.com>
2015-11-22 01:27:30 -0200

    Correção das headers

--- config/app_config.js
+++ config/app_config.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 app.use(function(req, res, next){
 	res.setHeader('Access-Control-Allow-Origin','*');
-	res.setHeader('Access-Control-Allow-Origin', 'GET,POST, PUT, DELETE');
-	res.setHeader('Access-Control-Allow-Origin', 'X-Requested-With,content-type, Authorization');
+	res.setHeader('Access-Control-Allow-Methods', 'GET,POST, PUT, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
 	next();
 })
